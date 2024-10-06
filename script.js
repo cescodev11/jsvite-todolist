7bfcf794f266cf10cc470527ad7da5907848c1c3
Carlos Escobar <carlosWebDev11@gmail.com>
2024-10-05 21:43:55 -0700

    save-edit

--- script.js
+++ script.js
@@ -7,13 +7,58 @@ console.log(itemsArray);
 document.querySelector("#enter").addEventListener("click", () => {
   const item = document.querySelector("#item");
   createItem(item);
-}); 
+});
 
-function displayItems() { 
-    let items = ""
-    for(let i = 0 ; i < itemsArray.length; i++) { 
-        
-    }
+function displayItems() {
+  let items = "";
+  for (let i = 0; i < itemsArray.length; i++) {
+    items += ` <div class="item">
+      <div class="input-controller">
+            <textarea disabled>${itemsArray[i]}</textarea>
+            <div class="edit-controller">
+              <i class="fa-solid fa-check deleteBtn"></i>
+              <i class="fa-solid fa-pen-to-square editBtn"></i>
+            </div>
+      </div>
+      <div class="update-controller">
+        <button class="saveBtn">Save</button>
+        <button class="cancelBtn">Cancel</button>
+
+      </div>
+    </div>`;
+  }
+  document.querySelector(".to-do-list").innerHTML = items;
+  activateDeleteListeners();
+    activateEditListeners()
+  //   activateSaveListeners()
+  //   activateCancelListeners()
+}
+
+function activateDeleteListeners() {
+  let deleteBtn = document.querySelectorAll(".deleteBtn");
+  deleteBtn.forEach((db, i) => {
+    db.addEventListener("click", () => {
+      deleteItem(i);
+    });
+  });
+}
+function activateEditListeners () { 
+    const editBtn = document.querySelectorAll(".editBtn")
+    const updateController = document.querySelectorAll(".update-controller")
+    const inputs = document.querySelectorAll(".input-controller textarea")
+    editBtn.forEach((eb,i)=> { 
+        eb.addEventListener("click", ()=> { 
+            updateController[i].style.display = "block"
+            inputs[i].disabled = false
+        })
+    })
+}
+
+
+function deleteItem(i) {
+  itemsArray.splice(i, 1);
+  localStorage.setItem("items", JSON.stringify(itemsArray));
+  location.reload();
 }
 
 function createItem(item) {
@@ -31,4 +76,5 @@ function displayDate() {
 
 window.onload = function () {
   displayDate();
+  displayItems();
 };
