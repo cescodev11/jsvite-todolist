af24c26bf9568f1cfe1a32c8a4ad6962c18a77cd
Carlos Escobar <carlosWebDev11@gmail.com>
2024-10-10 14:26:29 -0700

    updated-controller

--- script.js
+++ script.js
@@ -29,8 +29,8 @@ function displayItems() {
   }
   document.querySelector(".to-do-list").innerHTML = items;
   activateDeleteListeners();
-    activateEditListeners()
-  //   activateSaveListeners()
+  activateEditListeners();
+  activateSaveListeners();
   //   activateCancelListeners()
 }
 
@@ -42,18 +42,33 @@ function activateDeleteListeners() {
     });
   });
 }
-function activateEditListeners () { 
-    const editBtn = document.querySelectorAll(".editBtn")
-    const updateController = document.querySelectorAll(".update-controller")
-    const inputs = document.querySelectorAll(".input-controller textarea")
-    editBtn.forEach((eb,i)=> { 
-        eb.addEventListener("click", ()=> { 
-            updateController[i].style.display = "block"
-            inputs[i].disabled = false
-        })
-    })
+function activateEditListeners() {
+  const editBtn = document.querySelectorAll(".editBtn");
+  const updateController = document.querySelectorAll(".update-controller");
+  const inputs = document.querySelectorAll(".input-controller textarea");
+  editBtn.forEach((eb, i) => {
+    eb.addEventListener("click", () => {
+      updateController[i].style.display = "block";
+      inputs[i].disabled = false;
+    });
+  });
 }
 
+function activateSaveListeners() {
+  const saveBtn = document.querySelectorAll(".saveBtn");
+  const inputs = document.querySelectorAll(".input-controller textarea");
+  saveBtn.forEach((sb, i) => {
+    sb.addEventListener("click", () => {
+      updateItem(inputs[i].value, i);
+    });
+  });
+}
+
+function updateItem(text, i) {
+  itemsArray[i] = text;
+  localStorage.setItem("items", JSON.stringify(itemsArray));
+  location.reload();
+}
 
 function deleteItem(i) {
   itemsArray.splice(i, 1);
